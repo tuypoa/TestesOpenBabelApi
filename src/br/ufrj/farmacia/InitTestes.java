ca10d27d9ef9a684d6bcccfdd831e96efe9947f3
tuypoa <tuypoa@debian>
2020-06-26 12:59:34 -0300

    Config java/eclipse.

---
+++ src/br/ufrj/farmacia/InitTestes.java
@@ -0,0 +1,14 @@
+package br.ufrj.farmacia;
+
+public class InitTestes {
+
+	
+	public static void main(String a[]) {
+		System.out.println("Iniciando...");
+		
+		
+		
+		System.out.println("FIM.");
+	}
+	
+}
